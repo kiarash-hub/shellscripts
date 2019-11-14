8c941198f8b3d3a90ed49f13931fc6501b30fe3e
root <root@localhost.localdomain>
2019-11-14 10:46:22 -0800

    First commit

---
+++ familydatabase.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+while true
+do
+clear
+echo -e "What would you like to do?
+Add an entry (a)
+Search an entry (s)
+Quit (q)
+Enter your choice (a/s/q)-->\c"
+read ANSWER
+case $ANSWERn in
+a|A ) echo -e "Name of the family member --> \c"
+read NAME
+echo -e "Family member's relation to you -->\c" read RELATION
+echo -e "Family member's telephone number --> \c" read PHONE
+echo "$NAME\t$RELATION\t$PHONE" >> DATABASE
+;;
+s|S ) echo "What word would you like to look for? --> \c"
+read Word
+grep "$WORD" database
+sleep 4
+;;
+q|Q ) exit
+;;
+*)  echo "You must enter either the letter a or s."
+sleep 4
+;;
+esac
+done
+
